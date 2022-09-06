db1642ff1968896cc1e1265a34f1962eb1023815
JUFFLER <juffler@localhost.localdomain>
2022-09-06 14:42:56 -0400

    first

---
+++ main.js
@@ -0,0 +1,393 @@
+const express = require('express')
+const moment = require('moment')
+const mongoose = require('mongoose')
+const path = require('path')
+const {Schema} = mongoose
+
+// some local files 
+const auth = require(path.join(__dirname,"/credentials/auth"))
+
+// connection to data base 
+async function connection(){
+	await mongoose.connect(auth.DATABASEURI)
+}
+connection().catch(err => console.log(`Hey JUFFLER this is database connectin error :${err}`))
+
+// specific variables 
+const App = express()
+const port = process.env.PORT || 3000
+
+
+// Some middlewares 
+// urlencoder is for using req.body parameter 
+App.use(express.urlencoded({extended:false}))
+App.use(express.json())
+
+
+// --------------------------------------- SCHEMA SECTION -----------------------------------
+// creating schema for movie post
+const moviePost =  new Schema({
+	type: String,
+	name: String,
+	size: String,
+	image: String,
+	caption: String,
+	movieID:String,
+	releaseDate:{type:Date,default:Date.now},
+	next:String,
+	previous:String,
+	isSeries:{type:Boolean,default:false}
+})
+// submitting schema into modal of posts collection of moviesPost database
+const newMoviePost = mongoose.model('post', moviePost);
+
+// schema for payment webhook 
+const primeUser =  new Schema({
+	email:{type:String,required:true},
+	deviceID:{type:String,default:""},
+	wishList:{type:[String],default:[]},
+	amount:Number,
+	expireAt:{type:Date,default:undefined},
+	createdAt:{type:String,default:String(moment().format())}
+})
+// making exireAt field get expire with date time 
+primeUser.index({"expireAt":1},{expireAfterSeconds:0})
+// submitting schema into modal of user collection in primeUser database
+const newPrimeUser = mongoose.model('user', primeUser);
+
+
+//------------------------- primeUser API Routs webhook----------------------------------
+App.post("/savePayment",async(req,res)=>{
+	try {
+		data = req.body
+		if(data.status == "success"){
+			existance = await newPrimeUser.findOne({"email":data.email}).exec()
+			if(!existance){
+				const newPaidUser = newPrimeUser({
+					email:data.email,
+					deviceID:data.deviceID,
+					wishList:data.wishList,
+					amount:data.amount,
+					expireAt:moment().add(30,"days")
+				})
+				newPaidUser.save((err,doc)=>{
+					if(!err){
+						console.log("new  PrimeUser listed successFully ",doc)
+						return res.json({"status":true})
+					}else{
+						console.log("new User not listed because : ",err)
+						return res.json({"status":false})
+					}
+				})
+			}else{
+					newPrimeUser.updateOne({_id:existance._id},{"expireAt":moment(existance.expireAt).add(30,"days"),"amount":existance.amount+20},(err,data)=>{
+					if(!err){
+						console.log(`More 30 days has been added to ${existance.email} email address`)
+						return res.json({"status":true})
+					}else{
+						console.log("New subscription failed ",err)
+						return res.json({"status":false})
+					}
+				})
+
+			}
+		}else{
+			console.log(`Status for ${data.email} is ${data.status}`)
+			return res.json({"status":false})
+		}
+	
+	} catch(e) {
+		// statements
+		console.log(e);
+		return res.json({"status":false,"discreption":"Your request method is wrong read docs"})
+	}
+})
+// checkApi checks takes email and device id as body parameter and set device id according to email 
+App.post("/checkApi",async(req ,res)=>{
+	try {
+		data = req.body
+		existance = await newPrimeUser.findOne({"email":data.email}).exec()
+		if(!existance){
+			return res.json({"stauts":0 , "discreption":"user is not primuim"})
+		}else{
+			if(existance.deviceID.trim().lenght === 0 || existance.deviceID == data.deviceID){
+					newPrimeUser.updateOne({_id:existance._id},{"deviceID":data.deviceID},(err,data)=>{
+						if(!err){
+							console.log(`Device id is set for ${existance.email}`)
+							return res.json({"status":1 , "discreption": "device id is added successFully or same as previously"})
+						}else{
+							console.log(`Database error to update deviceID for ${existance.email} address`)
+							return res.json({"status":4,"discreption":"Database error to update deviceID"})
+						}
+					})
+			}else{
+				console.log(`DeviceId already exists which is different for ${existance.email}`)
+				return res.json({"status":2,"descreption":`DeviceId already exists which is different for ${existance.email}`})
+			}
+		}
+
+	} catch(e) {
+		// statements
+		console.log(e);
+		return res.json({"status":false,"discreption":"Your request method is wrong read docs"});
+	}
+
+})
+// logout method takes key as parameter and take email as query and then remove deviceID from that account 
+App.get("/logout/:key",async(req,res)=>{
+	data = req.query
+	if(req.params.key === auth.key){
+		existance = await newPrimeUser.findOne({"email":data.email}).exec()
+		if(!existance){
+			return res.json({"status":false,"descreption":`No premium account found with ${data.email}`})
+		}else{
+			if(existance.deviceID.trim().length === 0){
+				return res.json({"status":false,"descreption":`${data.email} address is not logged in any device please login first `})	
+			}else{
+				if(existance.deviceID == data.deviceID){
+					newPrimeUser.updateOne({_id:existance._id},{"deviceID":""},(err,data)=>{
+							if(!err){
+								console.log(`Device logout success for ${existance.email}`)
+								return res.json({"status":true , "discreption": "device is logged out successFully"})
+							}else{
+								console.log(`Database error to logout deviceID for ${existance.email} address`)
+								return res.json({"status":false,"discreption":"Database error to logout deviceID"})
+							}
+						})
+				}else{
+					res.json({"status":false,"discreption":"Your device id is not as same as in database"})
+				}
+
+			}
+		}
+	}else{
+		res.json({"status":false,"discreption":"Yout can't access api without key"})
+	}
+
+})
+// -----------------routs for crud operations in posts collection of moviesPost database-----------
+// default gateway 
+App.get("/",async(req,res)=>{
+	res.json({"status":"running"})
+})
+
+// Creating posts 
+App.post('/create/:key', async (req, res) => {
+	try {
+		data = req.body
+		
+		if(req.params.key === auth.key){
+			if(data.releaseDate){
+				const newPost = newMoviePost({
+			  	type: data.type,
+			  	name: data.name,
+			  	size: data.size,
+			  	image: data.image,
+			  	caption:data.caption,
+			  	releaseDate:data.releaseDate,
+			  	movieID:data.movieID,
+			  	next:data.next,
+			  	previous:data.previous,
+			  	isSeries:data.isSeries
+			  })
+				newPost.save((err, doc)=>{
+				  	if(!err){
+				  		return res.json({"stauts":true,"discreption":`Movie post successFully saved  with ${doc} details `})
+				  	}else{
+				  		console.log(`Error while saving movie post  ${err}`)
+				  		return res.json({"status":false,"discreption":`ERROR whiles saving : ${err}`})
+				  	}
+				  })
+			}else{
+				const newPost = newMoviePost({
+			  	type: data.type,
+			  	name: data.name,
+			  	size: data.size,
+			  	image: data.image,
+			  	caption:data.caption,
+			  	movieID:data.movieID,
+			  	next:data.next,
+			  	previous:data.previous,
+			  	isSeries:data.isSeries
+			  })
+				newPost.save((err, doc)=>{
+				  	if(!err){
+				  		return res.json({"stauts":true,"discreption":`Movie post successFully saved  with ${doc} details `})
+				  	}else{
+				  		console.log(`Error while saving movie post  ${err}`)
+				  		return res.json({"status":false,"discreption":`ERROR whiles saving : ${err}`})
+				  	}
+				  })
+
+			}
+		}else{
+			return res.json({"status":false,"descreption":"you must pass key as a parameter"})
+		}
+	} catch(e) {
+		
+		console.log(e);
+		return res.json({"status":false,"discreption":"Your request method is wrong read docs"})
+	}
+  
+})
+// reading posts 
+App.get('/read/:key',async(req,res)=>{
+	const data = req.query
+	console.log(data.isSeries)
+	var contentList = []
+	if(data.id){
+			newMoviePost.findById(data.id,(err,doc)=>{
+				if(!err){
+					return res.json({"status":true,"data":doc})
+				}else{
+					console.log(err)
+					return res.json({"status":false,"discreption":"No id found. But you can still contact JUFFLER"})
+
+				}
+			})
+	}else{
+			if(data.isSeries == true){
+					// -----------------------------------------including Series ---------------------------
+				// getting data from various parameters 
+				if(data.type){
+					// if data.type is like this {"type":["anime","jumanju"]}
+					if(data.type instanceof Array){
+						const dataFromTypeAndName = await newMoviePost.find({"type":data.type[0],"name":{"$regex":data.type[1],"$options":"i"}}).sort({releaseDate: 'descending'}).exec()
+						contentList = contentList.concat(JSON.parse(JSON.stringify(dataFromTypeAndName)))	
+					}else{
+						const dataFromType = await newMoviePost.find({"type":data.type}).sort({releaseDate: 'descending'}).exec()
+						contentList = contentList.concat(JSON.parse(JSON.stringify(dataFromType)))
+					}
+				}
+				if(data.name){
+					const dataFromName = await newMoviePost.find({"name":{"$regex":data.name,"$options":"i"}}).sort({releaseDate: 'descending'}).exec()
+					contentList = contentList.concat(JSON.parse(JSON.stringify(dataFromName)))
+				}
+				// if no filter seach is not used so adding all data 
+				if(!data.type && !data.name && !data.id){
+					// else reading all posts 
+					const allData = await newMoviePost.find().sort({releaseDate: 'descending'}).exec()
+					console.log(allData)
+					contentList = contentList.concat(JSON.parse(JSON.stringify(allData)))
+				}
+			}else{
+				// --------------------excluding series ---------------------------
+				// getting data from various parameters 
+				if(data.type){
+					// if data.type is like this {"type":["anime","jumanju"]}
+					if(data.type instanceof Array){
+						const dataFromTypeAndName = await newMoviePost.find({"isSeries":false,"type":data.type[0],"name":{"$regex":data.type[1],"$options":"i"}}).sort({releaseDate: 'descending'}).exec()
+						contentList = contentList.concat(JSON.parse(JSON.stringify(dataFromTypeAndName)))	
+					}else{
+						const dataFromType = await newMoviePost.find({"isSeries":false,"type":data.type}).sort({releaseDate: 'descending'}).exec()
+						contentList = contentList.concat(JSON.parse(JSON.stringify(dataFromType)))
+					}
+				}
+				if(data.name){
+					const dataFromName = await newMoviePost.find({"isSeries":false,"name":{"$regex":data.name,"$options":"i"}}).sort({releaseDate: 'descending'}).exec()
+					contentList = contentList.concat(JSON.parse(JSON.stringify(dataFromName)))
+				}
+				// if no filter seach is not used so adding all data 
+				if(!data.type && !data.name ){
+					// else reading all posts 
+					const allData = await newMoviePost.find({"isSeries":false}).sort({releaseDate: 'descending'}).exec()
+					contentList = contentList.concat(JSON.parse(JSON.stringify(allData)))
+				}
+			}
+		
+			// removing duplicates from contentList
+			const uniqueIds = []
+			const filteredContentList = contentList.filter(element=>{
+				const isDuplicated = uniqueIds.includes(element._id)
+				if(!isDuplicated){
+					uniqueIds.push(element._id)
+					return true
+				}
+				return false
+			})
+
+			// checking if we get any data 
+			if(filteredContentList.length === 0){
+				return res.json({"status":false,"discreption":"NO results found for your search Query. May be your database is empty Try make request with no filter parameters"})
+			}else{
+				return res.json({"stauts":true,"data":filteredContentList ,"size": filteredContentList.length})
+			}
+	}
+})
+App.put("/update/:key", async(req,res)=>{
+	try {
+		data = req.body
+		if(req.params.key === auth.key){
+			if(data.id){
+				if(data.fields){
+					// making python string to json 
+					const toUpdateFields = JSON.parse(data.fields)
+					const newDocument = await newMoviePost.findByIdAndUpdate(data.id,{"$set":toUpdateFields}).exec()
+					return res.json({"status":true,"discreption":`field  is updated to ${newDocument}`})
+				}else{
+					return res.json({"status":false,"discreption":`field  is ${data.fields}. Please provide some fields to update e.g {"name":"Some new name"}`})
+				}
+			}else{
+				return res.json({"stauts":false,"descreption":"you must pass Id to update in body (data)"})	
+			}
+		}else{
+			return res.json({"stauts":false,"descreption":"you must pass key as a parameter"})
+		}
+	} catch(e) {
+		// statements
+		console.log(e);
+		return res.json({"status":false,"discreption":"Your request method is wrong read docs"})
+	}
+})
+App.delete('/deleteOne/:key', async(req,res)=>{
+	try {
+		data = req.body
+		if(req.params.key === auth.key){
+			if(data.id){
+					newMoviePost.findByIdAndDelete(data.id,(err,doc)=>{
+						if(!err){
+							return res.json({"status":true,"discreption":`ho gya delete ya : ${doc}`})
+						}else{
+							console.log(err)
+							return res.json({"status":false , "discreption":"Your given id looks wrong. But you can still contact JUFFLER for more details"})
+						}
+					})
+			}else{
+				return res.json({"stauts":false,"descreption":"you must pass Id to delete for in body (data)"})	
+			}
+		}else{
+			return res.json({"stauts":false,"descreption":"you must pass key as a parameter"})
+		}
+	} catch(e) {
+		// statements
+		console.log(e);
+		return res.json({"status":false,"discreption":"Your request method is wrong read docs"})
+	}
+})
+// advance operation functions 
+App.delete('/deleteAll/:key', async (req, res) => {
+	try {
+		if(req.params.key === auth.key){
+		  	newMoviePost.deleteMany({},(err, doc)=>{
+		  	if(!err){
+		  		return res.json({"stauts":true,"descreption":"all posts deleted successFully"})
+		  	}else{
+		  		console.log(`this is error ${err}`)
+		  		return res.json({"stauts":false,"descreption":"Database error while deleating all posts"})
+		  	}
+		  })
+		}else{
+			return res.json({"stauts":false,"descreption":"you must pass key as a parameter"})
+		}
+	} catch(e) {
+		console.log(e);
+		return res.json({"status":false,"discreption":"Your request method is wrong read docs"})
+	}
+  
+})
+
+
+
+App.listen(port , ()=>{
+	console.log(`App listening on :${port}`)
+})
